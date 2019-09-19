a2dff4ed02127820804198e93782a15b060f94d0
Tatiana Lytvyn <litvin.tatianka@gmail.com>
2019-09-19 17:19:38 +0300

    ok

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -18,11 +18,11 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <!--metatextblock-->
     <title>Натуральное кокосовое мыло Maldives Dreams</title>
-    <link rel="canonical" href="http://soap.maldivesdreams.com.ua">
     <!-- Assets -->
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
