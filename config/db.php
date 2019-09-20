b97c5b39e7819b147c09b0ee68dc574417dc7c94
Tatiana Lytvyn <litvin.tatianka@gmail.com>
2019-09-20 10:25:15 +0300

    update db

--- config/db.php
+++ config/db.php
@@ -1,14 +1,15 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=soap_maldive',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'mysql:host=localhost;dbname=soap_maldive',
+//    'username' => 'root',
+//    'password' => '',
+//    'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=oniks01.mysql.tools;dbname=oniks01_soap',
+    'username' => 'oniks01_soap',
+    'password' => '2K!9gi2a!X',
+    'charset' => 'utf8',
 ];
